bca5d4d2edab7af5337b276b0f1b9d3209437778
xyxiao001 <463618622@qq.com>
2021-12-05 11:50:47 +0800

    feat: 添加第一部分例子

---
+++ studygolagn/chapter-one/1.5-url/url.go
@@ -0,0 +1,28 @@
+// go run url.go http://xyxiao.cn
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		fmt.Printf("开始读取网站: %s\n", url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
