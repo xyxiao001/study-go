81c9b842041ce1cac607a23824f7a5981f89283f
xyxiao001 <463618622@qq.com>
2022-02-07 14:59:11 +0800

    feat: 新增

---
+++ studygolang/chapter-2/2.5/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"test/temperature"
+)
+
+func main() {
+	fmt.Printf("%g\n", temperature.BoilingC)
+}
---
+++ studygolang/chapter-2/2.5/temperature/temperature.go
@@ -0,0 +1,17 @@
+package temperature
+
+type Celsius float64    // 摄氏温度
+type Fahrenheit float64 // 华氏温度
+
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点
+	BoilingC      Celsius = 100     // 沸水温度
+)
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+func FToC(f Fahrenheit) Celsius {
+	return Celsius(f-32) * 5 / 9
+}
